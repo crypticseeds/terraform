bb9ff0e30c3cff0ef32dc7d4606105af64f604c0
Femi Akinlotan <94658643+crypticseeds@users.noreply.github.com>
2024-09-09 17:09:48 -0400

    updated ec2 module

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,41 +1,103 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "tag_name" {}
-variable "public_key" {}
 variable "subnet_id" {}
-variable "sg_enable_ssh_https" {}
-variable "enable_public_ip_address" {}
-variable "user_data_install_apache" {}
-variable "ec2_sg_name_for_python_api" {}
-
-output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "ssh -i /home/ubuntu/keys/aws_ec2_terraform ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
+variable "security_group_id" {}
+variable "instance_type" {}
+variable "ec2_ami_owner" {}
+variable "ec2_ami_name_filter" {}
+variable "public_key" {
+  type        = string
+  description = "Public key for EC2 instance SSH access"
 }
 
-output "dev_proj_1_ec2_instance_id" {
-  value = aws_instance.dev_proj_1_ec2.id
-}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = [var.ec2_ami_owner]
 
-resource "aws_instance" "dev_proj_1_ec2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  tags = {
-    Name = var.tag_name
+  filter {
+    name   = "name"
+    values = [var.ec2_ami_name_filter]
   }
-  key_name                    = "aws_key"
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [var.sg_enable_ssh_https, var.ec2_sg_name_for_python_api]
-  associate_public_ip_address = var.enable_public_ip_address
 
-  user_data = var.user_data_install_apache
-
-  metadata_options {
-    http_endpoint = "enabled"  # Enable the IMDSv2 endpoint
-    http_tokens   = "required" # Require the use of IMDSv2 tokens
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
-resource "aws_key_pair" "dev_proj_1_public_key" {
-  key_name   = "aws_key"
+resource "aws_key_pair" "main" {
+  key_name   = "main-key-pair"
   public_key = var.public_key
 }
+
+resource "aws_instance" "jenkins" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  key_name                    = aws_key_pair.main.key_name
+  associate_public_ip_address = true  
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
+
+
+# Uncomment the following blocks to launch additional EC2 instances
+
+/*
+resource "aws_instance" "sonarqube" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name                    = aws_key_pair.main.key_name
+  associate_public_ip_address = true  
+  tags = {
+    Name = "Sonarqube-Server"
+  }
+ }
+
+output "sonarqube_public_ip" {
+  value = aws_instance.sonarqube.public_ip
+}
+*/
+
+/*
+resource "aws_instance" "argocd" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name                    = aws_key_pair.main.key_name
+  associate_public_ip_address = true  
+  tags = {
+    Name = "ArgoCD-Server"
+  }
+}
+
+output "argocd_public_ip" {
+  value = aws_instance.argocd.public_ip
+}
+*/
+
+/*
+resource "aws_instance" "nexus" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+  key_name                    = aws_key_pair.main.key_name
+  associate_public_ip_address = true  
+  tags = {
+    Name = "Nexus-Server"
+  }
+}
+
+output "nexus_public_ip" {
+  value = aws_instance.nexus.public_ip
+}
+*/
