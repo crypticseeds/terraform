c305efa75a887dd3275e83f6e16b3e24c8f12f4a
Femi Akinlotan <94658643+crypticseeds@users.noreply.github.com>
2024-09-09 17:12:23 -0400

    updated output file

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,31 @@
-output "dev_proj_1_vpc_id" {
-  value = module.networking.dev_proj_1_vpc_id
+output "vpc_id" {
+  value = module.networking.vpc_id
 }
 
-/*output "ec2_ssh_string" {
-  value = module.ec2.ssh_connection_string_for_ec2
+output "jenkins_public_ip" {
+  value = module.ec2.jenkins_public_ip
 }
 
-output "hosted_zone_id" {
-  value = module.hosted_zone.hosted_zone_id
-}*/
+# Commented out outputs for resources not currently in use
+/*
+output "sonarqube_public_ip" {
+  value = module.ec2.sonarqube_public_ip
+}
+
+output "argocd_public_ip" {
+  value = module.ec2.argocd_public_ip
+}
+
+output "nexus_public_ip" {
+  value = module.ec2.nexus_public_ip
+}
+
+output "lb_dns_name" {
+  value = module.lb.nlb_dns_name
+}
+
+*/
+
+output "eks_cluster_endpoint" {
+  value = module.eks.endpoint
+}
